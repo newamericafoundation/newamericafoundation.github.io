03e07e785e53eed6ea7072097f6f60a054e05d07
Nick McClellan <mcclellan.nick@gmail.com>
2012-02-24 13:01:52 -0500

    Nick's unfinished work from Friday

--- assets/js/custom/retirement.js
+++ assets/js/custom/retirement.js
@@ -47,3 +47,469 @@ $(document).ready(function() {
 		}]
 	});
 });
+
+var chart;
+  $(document).ready(function() {
+	 chart = new Highcharts.Chart({
+		chart: {
+		   renderTo: 'top-quartile',
+		   plotBackgroundColor: null,
+		   plotBorderWidth: null,
+		   spacingRight:100
+		},
+		title: {
+		   text: 'Top quartile'
+		},
+		credits: {
+			enabled: false	
+		},
+		exporting: {
+			enabled: false	
+		},
+		legend: {
+			enabled: false	
+		},
+		yAxis: {
+			title: {
+				text: null
+			},
+			labels: {
+			  formatter: function() {
+				return this.value +'%';
+			  }
+			},
+			max:90,
+			min:30,
+//			tickInterval: 75,
+		},
+		xAxis: {
+//			labels: {
+//				enabled: false	
+//			},
+//			endOnTick: true,
+//			startOnTick: true,
+			categories: ["'00", "'08"]
+		},
+		tooltip: {
+			style: {
+				fontSize: '9px',	
+			},
+			formatter: function() {
+			  var s;
+			  if (this.point.name) { // the pie chart
+				 s = ''+
+					this.point.name +': '+ this.y + '%';
+			  } else {
+				 s = ''+
+					this.series.name  +': '+ this.y;
+			  }
+			  return s;
+		   }
+		},
+		plotOptions: {
+			line: {
+				pointWidth: 25,
+				pointPadding: .5,
+				groupPadding: .3,
+				minPointLength: 1,
+			}
+		},
+		labels: {
+			items: [{
+				html: '2000',
+				style: {
+					left: '40px',
+					top: '8px',
+					color: 'black'
+				}
+			}]
+		},
+		series: [{
+		   type: 'pie',
+		   name: 'Employee retirement plans',
+		   data: [{
+			  name: 'Employee participation',
+			  y: 75.5,
+			  color: '#660033'
+		   }, {
+			  name: 'No participation',
+			  y: 24.5,
+			  color: '#EEEEEE'
+		   }],
+		   center: [60, 30],
+		   spacingLeft: 100,
+		   size: 60,
+		   showInLegend: false,
+		   dataLabels: {
+			  enabled: false
+		   },
+		   borderColor: '#000000',
+		},{
+		   type: 'pie',
+		   name: 'Employee retirement plans',
+		   data: [{
+			  name: 'Employee participation',
+			  y: 68.6,
+			  color: '#660033'
+		   }, {
+			  name: 'No participation',
+			  y: 31.4,
+			  color: '#EEEEEE'
+		   }],
+		   center: [550, 30],
+		   spacingLeft: 100,
+		   size: 60,
+		   showInLegend: false,
+		   dataLabels: {
+			  enabled: false
+		   },
+		   borderColor: '#000000',
+		},{
+			type: 'line',
+			data: [80.2, 72.9],
+			color: '#0D253F',
+		}]
+	 });
+  });
+
+var chart;
+  $(document).ready(function() {
+	 chart = new Highcharts.Chart({
+		chart: {
+		   renderTo: 'second-quartile',
+		   plotBackgroundColor: null,
+		   plotBorderWidth: null,
+		   spacingRight:100
+		},
+		title: {
+		   text: 'Top quartile'
+		},
+		credits: {
+			enabled: false	
+		},
+		exporting: {
+			enabled: false	
+		},
+		legend: {
+			enabled: false	
+		},
+		yAxis: {
+			title: {
+				text: null
+			},
+			labels: {
+			  formatter: function() {
+				return this.value +'%';
+			  }
+			},
+			max:90,
+			min:30,
+//			tickInterval: 75,
+		},
+		xAxis: {
+//			labels: {
+//				enabled: false	
+//			},
+//			endOnTick: true,
+//			startOnTick: true,
+			categories: ["'00", "'08"]
+		},
+		tooltip: {
+			style: {
+				fontSize: '9px',	
+			},
+			formatter: function() {
+			  var s;
+			  if (this.point.name) { // the pie chart
+				 s = ''+
+					this.point.name +': '+ this.y + '%';
+			  } else {
+				 s = ''+
+					this.series.name  +': '+ this.y;
+			  }
+			  return s;
+		   }
+		},
+		plotOptions: {
+			line: {
+				pointWidth: 25,
+				pointPadding: .5,
+				groupPadding: .3,
+				minPointLength: 1,
+			}
+		},
+		series: [{
+		   type: 'pie',
+		   name: 'Employee retirement plans',
+		   data: [{
+			  name: 'Employee participation',
+			  y: 75.5,
+			  color: '#660033'
+		   }, {
+			  name: 'No participation',
+			  y: 24.5,
+			  color: '#EEEEEE'
+		   }],
+		   center: [60, 30],
+		   spacingLeft: 100,
+		   size: 60,
+		   showInLegend: false,
+		   dataLabels: {
+			  enabled: false
+		   },
+		   borderColor: '#000000',
+		},{
+		   type: 'pie',
+		   name: 'Employee retirement plans',
+		   data: [{
+			  name: 'Employee participation',
+			  y: 68.6,
+			  color: '#660033'
+		   }, {
+			  name: 'No participation',
+			  y: 31.4,
+			  color: '#EEEEEE'
+		   }],
+		   center: [550, 30],
+		   spacingLeft: 100,
+		   size: 60,
+		   showInLegend: false,
+		   dataLabels: {
+			  enabled: false
+		   },
+		   borderColor: '#000000',
+		},{
+			type: 'line',
+			data: [80.2, 72.9],
+			color: '#0D253F',
+		}]
+	 });
+  });
+
+var chart;
+  $(document).ready(function() {
+	 chart = new Highcharts.Chart({
+		chart: {
+		   renderTo: 'third-quartile',
+		   plotBackgroundColor: null,
+		   plotBorderWidth: null,
+		   spacingRight:100
+		},
+		title: {
+		   text: 'Top quartile'
+		},
+		credits: {
+			enabled: false	
+		},
+		exporting: {
+			enabled: false	
+		},
+		legend: {
+			enabled: false	
+		},
+		yAxis: {
+			title: {
+				text: null
+			},
+			labels: {
+			  formatter: function() {
+				return this.value +'%';
+			  }
+			},
+			max:90,
+			min:30,
+//			tickInterval: 75,
+		},
+		xAxis: {
+//			labels: {
+//				enabled: false	
+//			},
+//			endOnTick: true,
+//			startOnTick: true,
+			categories: ["'00", "'08"]
+		},
+		tooltip: {
+			style: {
+				fontSize: '9px',	
+			},
+			formatter: function() {
+			  var s;
+			  if (this.point.name) { // the pie chart
+				 s = ''+
+					this.point.name +': '+ this.y + '%';
+			  } else {
+				 s = ''+
+					this.series.name  +': '+ this.y;
+			  }
+			  return s;
+		   }
+		},
+		plotOptions: {
+			line: {
+				pointWidth: 25,
+				pointPadding: .5,
+				groupPadding: .3,
+				minPointLength: 1,
+			}
+		},
+		series: [{
+		   type: 'pie',
+		   name: 'Employee retirement plans',
+		   data: [{
+			  name: 'Employee participation',
+			  y: 75.5,
+			  color: '#660033'
+		   }, {
+			  name: 'No participation',
+			  y: 24.5,
+			  color: '#EEEEEE'
+		   }],
+		   center: [60, 30],
+		   spacingLeft: 100,
+		   size: 60,
+		   showInLegend: false,
+		   dataLabels: {
+			  enabled: false
+		   },
+		   borderColor: '#000000',
+		},{
+		   type: 'pie',
+		   name: 'Employee retirement plans',
+		   data: [{
+			  name: 'Employee participation',
+			  y: 68.6,
+			  color: '#660033'
+		   }, {
+			  name: 'No participation',
+			  y: 31.4,
+			  color: '#EEEEEE'
+		   }],
+		   center: [550, 30],
+		   spacingLeft: 100,
+		   size: 60,
+		   showInLegend: false,
+		   dataLabels: {
+			  enabled: false
+		   },
+		   borderColor: '#000000',
+		},{
+			type: 'line',
+			data: [80.2, 72.9],
+			color: '#0D253F',
+		}]
+	 });
+  });
+
+var chart;
+  $(document).ready(function() {
+	 chart = new Highcharts.Chart({
+		chart: {
+		   renderTo: 'bottom-quartile',
+		   plotBackgroundColor: null,
+		   plotBorderWidth: null,
+		   spacingRight:100
+		},
+		title: {
+		   text: 'Top quartile'
+		},
+		credits: {
+			enabled: false	
+		},
+		exporting: {
+			enabled: false	
+		},
+		legend: {
+			enabled: false	
+		},
+		yAxis: {
+			title: {
+				text: null
+			},
+			labels: {
+			  formatter: function() {
+				return this.value +'%';
+			  }
+			},
+			max:90,
+			min:30,
+//			tickInterval: 75,
+		},
+		xAxis: {
+//			labels: {
+//				enabled: false	
+//			},
+//			endOnTick: true,
+//			startOnTick: true,
+			categories: ["'00", "'08"]
+		},
+		tooltip: {
+			style: {
+				fontSize: '9px',	
+			},
+			formatter: function() {
+			  var s;
+			  if (this.point.name) { // the pie chart
+				 s = ''+
+					this.point.name +': '+ this.y + '%';
+			  } else {
+				 s = ''+
+					this.series.name  +': '+ this.y;
+			  }
+			  return s;
+		   }
+		},
+		plotOptions: {
+			line: {
+				pointWidth: 25,
+				pointPadding: .5,
+				groupPadding: .3,
+				minPointLength: 1,
+			}
+		},
+		series: [{
+		   type: 'pie',
+		   name: 'Employee retirement plans',
+		   data: [{
+			  name: 'Employee participation',
+			  y: 75.5,
+			  color: '#660033'
+		   }, {
+			  name: 'No participation',
+			  y: 24.5,
+			  color: '#EEEEEE'
+		   }],
+		   center: [60, 30],
+		   spacingLeft: 100,
+		   size: 60,
+		   showInLegend: false,
+		   dataLabels: {
+			  enabled: false
+		   },
+		   borderColor: '#000000',
+		},{
+		   type: 'pie',
+		   name: 'Employee retirement plans',
+		   data: [{
+			  name: 'Employee participation',
+			  y: 68.6,
+			  color: '#660033'
+		   }, {
+			  name: 'No participation',
+			  y: 31.4,
+			  color: '#EEEEEE'
+		   }],
+		   center: [550, 30],
+		   spacingLeft: 100,
+		   size: 60,
+		   showInLegend: false,
+		   dataLabels: {
+			  enabled: false
+		   },
+		   borderColor: '#000000',
+		},{
+			type: 'line',
+			data: [80.2, 72.9],
+			color: '#0D253F',
+		}]
+	 });
+  });
