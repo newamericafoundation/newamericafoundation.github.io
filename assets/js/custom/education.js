47f1a6d7d15c998689c437695ca86e0ba89ad678
Andy Hull <andyhull3@gmail.com>
2012-02-28 11:49:14 -0500

    fixed pie charts

--- assets/js/custom/education.js
+++ assets/js/custom/education.js
@@ -1,57 +1,49 @@
 // JavaScript Document
-
-var chart;
-$(document).ready(function() {
-	chart = new Highcharts.Chart({
-		chart: {
-			renderTo: 'education-graph',
-			type: 'line',
-		},
-		title: {
-			text: 'Students enrolled in college after graduation',
-		},
-		subtitle: {
-			text: 'Percentage',
-		},
-		xAxis: {
-			categories: ['2005', '2006', '2007', '2008', '2009']
-		},
-		yAxis: {
-			title: {
-				text: null
-			},
-			plotLines: [{
-				value: 0,
-				width: 1,
-				color: '#808080'
-			}]
-		},
-		tooltip: {
-			formatter: function() {
-					return '<b>'+ this.series.name +'</b><br/>'+
-					this.x +': '+ this.y +'%';
-			}
-		},
-//		legend: {
-//			layout: 'vertical',
-//			align: 'right',
-//			verticalAlign: 'top',
-//			x: -10,
-//			y: 100,
-//			borderWidth: 0
-//		},
-		series: [{
-			name: 'High-income',
-			data: [81.2, 80.7, 78.2, 81.9, 84.2]
-		},{
-			name: 'Middle-income',
-			data: [65.1, 61.4, 63.3, 65.3, 66.8]
-		},{
-			name: 'Low-income',
-			data: [51, 54.5, 55.2, 56, 54]
-		}]
-	});
-});
+// 
+// var chart;
+// $(document).ready(function() {
+// 	chart = new Highcharts.Chart({
+// 		chart: {
+// 			renderTo: 'education-graph',
+// 			type: 'line',
+// 		},
+// 		title: {
+// 			text: 'Students enrolled in college after graduation',
+// 		},
+// 		subtitle: {
+// 			text: 'Percentage',
+// 		},
+// 		xAxis: {
+// 			categories: ['2005', '2006', '2007', '2008', '2009']
+// 		},
+// 		yAxis: {
+// 			title: {
+// 				text: null
+// 			},
+// 			plotLines: [{
+// 				value: 0,
+// 				width: 1,
+// 				color: '#808080'
+// 			}]
+// 		},
+// 		tooltip: {
+// 			formatter: function() {
+// 					return '<b>'+ this.series.name +'</b><br/>'+
+// 					this.x +': '+ this.y +'%';
+// 			}
+// 		},
+// 		series: [{
+// 			name: 'High-income',
+// 			data: [81.2, 80.7, 78.2, 81.9, 84.2]
+// 		},{
+// 			name: 'Middle-income',
+// 			data: [65.1, 61.4, 63.3, 65.3, 66.8]
+// 		},{
+// 			name: 'Low-income',
+// 			data: [51, 54.5, 55.2, 56, 54]
+// 		}]
+// 	});
+// });
 
 //var chart;
 //$(document).ready(function() {
