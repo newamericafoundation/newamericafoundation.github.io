cd5050c4432098e69ad8fbdf1ce599de64452f45
Andy Hull <andyhull3@gmail.com>
2012-03-01 14:59:42 -0500

    moved the tooltip

--- gap/js/mapInit.js
+++ gap/js/mapInit.js
@@ -20,7 +20,115 @@
 			m = new mm.Map(container,
 				new wax.mm.connector(tilejson));
 				
-	    		wax.mm.interaction(m, tilejson);
+	    		wax.mm.interaction(m, tilejson).remove();
+				wax.mm.interaction(m, tilejson, {
+				  callbacks:
+							{	// Show a tooltip.
+								over: function(feature, context) {
+
+								    if (!feature) return;
+								    context.style.cursor = 'pointer';
+
+								    if (this.isPopup(this._currentTooltip)) {
+								        return;
+								    } else {
+								        this._currentTooltip = this.getTooltip(feature, context)
+								    }
+									// buildChart()
+									// $('#chartContainer').show();
+								// $('.debt-gdp-timeline').children('span.chart').empty();
+								},
+								out: function(context) {
+								    context.style.cursor = 'default';
+
+								    if (this.isPopup(this._currentTooltip)) {
+								        return;
+								    } else if (this._currentTooltip) {
+								        this.hideTooltip(this._currentTooltip);
+								        this._currentTooltip = undefined;
+								    }
+								$('#toolTipContainer').empty();
+								// $('#chartContainer').hide();
+
+								},
+								isPopup: function(el) {
+								    return el && el.className.indexOf('wax-popup') !== -1;
+								},
+								getTooltip: function(feature, context) {
+								    var tooltip = document.createElement('div');
+								    tooltip.className = 'wax-tooltip wax-tooltip-0';
+								    tooltip.innerHTML = feature;
+									// $('.intro').css('display','none');
+
+								// $(tooltip.innerHTML).children('.debt-gdp-timeline').children('span.chart').empty();
+								$('#toolTipContainer').append(tooltip);
+								    // context.appendChild(tooltip);
+								    return tooltip;
+								},
+								hideTooltip: function(el) {
+								    if (!el) return;
+								    var event,
+								        remove = function() {
+								        if (this.parentNode) this.parentNode.removeChild(this);
+								    };
+
+								    if (el.style['-webkit-transition'] !== undefined && this.animationOut) {
+								        event = 'webkitTransitionEnd';
+								    } else if (el.style.MozTransition !== undefined && this.animationOut) {
+								        event = 'transitionend';
+								    }
+
+								    if (event) {
+								        // This code assumes that transform-supporting browsers
+								        // also support proper events. IE9 does both.
+								        el.addEventListener(event, remove, false);
+								        el.addEventListener('transitionend', remove, false);
+								        el.className += ' ' + this.animationOut;
+								    } else {
+								        if (el.parentNode) el.parentNode.removeChild(el);
+										// $('.intro').css('display','block');
+								    }
+								},
+								click: function(feature, context) {
+								    // Hide any current tooltips.
+								    if (this._currentTooltip) {
+								        this.hideTooltip(this._currentTooltip);
+								        this._currentTooltip = undefined;
+								    }
+
+								    var tooltip = this.getTooltip(feature, context);
+								    tooltip.className += ' wax-popup';
+								    tooltip.innerHTML = feature;
+
+								    var close = document.createElement('a');
+								    close.href = '#close';
+								    close.className = 'close';
+								    close.innerHTML = 'Close';
+								    tooltip.appendChild(close);
+
+								    var closeClick = wax.util.bind(function(ev) {
+								        this.hideTooltip(tooltip);
+								        this._currentTooltip = undefined;
+								        ev.returnValue = false; // Prevents hash change.
+								        if (ev.stopPropagation) ev.stopPropagation();
+								        if (ev.preventDefault) ev.preventDefault();
+								        return false;
+								    }, this);
+
+								    // IE compatibility.
+								    if (close.addEventListener) {
+								        close.addEventListener('click', closeClick, false);
+								    } else if (close.attachEvent) {
+								        close.attachEvent('onclick', closeClick);
+								    }
+
+								    this._currentTooltip = tooltip;
+								// buildChart()
+								// $('.debt-gdp-timeline').children('span.chart').empty();
+								// $('#chartContainer').show();
+								}
+							}
+				});
 		wax.mm.legend(m, tilejson).appendTo(m.parent);
 		wax.mm.zoomer(m, tilejson).appendTo(m.parent);	
 		if(userCenter && userZoom){
