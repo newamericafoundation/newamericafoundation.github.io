1304a1e4ed59e1a6aff269be8b28b7d928bf40bd
Nick McClellan <mcclellan.nick@gmail.com>
2012-05-16 17:00:31 -0400

    COB changes for Kenya

--- spinnaker/js/charts.js
+++ spinnaker/js/charts.js
@@ -8,7 +8,7 @@ Highcharts.theme = {
 	 text: null,
       style: {
          color: '#000',
-         font: '24px Helvetica, Arial, sans-serif',
+         font: '22px Helvetica, Arial, sans-serif',
 		 fontWeight: '100'
       }	 
   },
@@ -18,6 +18,34 @@ Highcharts.theme = {
          color: '#666',
          font: '16px Helvetica, Arial, sans-serif',
 		 fontWeight: '100'
+      }	 
+  },
+  legend: {
+      style: {
+         color: '#000',
+         font: 'Helvetica, Arial, sans-serif',
+      }	 
+  },
+  xAxis: {
+	  labels: {
+		  style: {
+		   color: '#000',
+		   font: 'Helvetica, Arial, sans-serif',
+		  }
+      }	 
+  },
+  yAxis: {
+	  labels: {
+		  style: {
+		   color: '#000',
+		   font: 'Helvetica, Arial, sans-serif',
+		  }
+      }	 
+  },
+  labels: {
+      style: {
+         color: '#000',
+         font: 'Helvetica, Arial, sans-serif',
       }	 
   },
   credits: {
@@ -32,6 +60,284 @@ var highchartsOptions = Highcharts.setOptions(Highcharts.theme);
 
 var chart;
 $(document).ready(function() {
+	chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'savings1',
+			plotBackgroundColor: null,
+			plotBorderWidth: null,
+			plotShadow: false
+		},
+		title: {
+			text: 'Type of Institutions Surveyed'
+		},
+		tooltip: {
+			formatter: function() {
+				return this.point.name;
+			}
+		},
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: true,
+					color: '#000000',
+					connectorColor: '#000000',
+					formatter: function() {
+						return this.point.name;
+					}
+				}
+			}
+		},
+		series: [{
+			type: 'pie',
+			name: 'Browser share',
+			data: [
+				['Commercial Bank',   12],
+				['Postal Bank',       1],
+				['Microfinance Institution',    4],
+				['Credit Union/Cooperative',     1],
+				['Insurance Provider',   2],
+				['State Owned Bank',   1]
+			]
+		}]
+	});
+
+var chart;
+	chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'savings2',
+			plotBackgroundColor: null,
+			plotBorderWidth: null,
+			plotShadow: false
+		},
+		title: {
+			text: 'Types of Savings Products'
+		},
+		tooltip: {
+			formatter: function() {
+				return this.point.name;
+			}
+		},
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: true,
+					color: '#000000',
+					connectorColor: '#000000',
+					formatter: function() {
+						return this.point.name;
+					}
+				}
+			}
+		},
+		series: [{
+			type: 'pie',
+			name: 'Browser share',
+			data: [
+				['Current Account',   75],
+				['Commitment Savings Account',       26],
+				['Recurring Standard Deposit Account',    3],
+			]
+		}]
+	});
+
+var chart;
+	chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'technology1',
+			type: 'line',
+		},
+		title: {
+		   text: 'Increasing Access to Delivery Points Across Kenya'
+		},
+		subtitle: {
+			text: null,
+		},
+		xAxis: {
+			categories: ['2007','2008','2009','2010']
+		},
+		yAxis: {
+			title: {
+				text: null
+			},
+			plotLines: [{
+				value: 0,
+				width: 1,
+				color: '#808080'
+			}]
+		},
+		tooltip: {
+			formatter: function() {
+					return '<b>'+ this.series.name +'</b><br/>'+
+					this.x +': '+ this.y;
+			}
+		},
+		series: [{
+			name: 'Commercial Bank Branches',
+			data: [740, 887, 990, 1016]
+		},{
+			name: 'MFI Branches',
+			data: [0, 0, 6, 47]
+		},{
+			name: '# ATMs',
+			data: [1012, 1325, 1717, 1979]
+		},{
+			name: 'Bank Agents',
+			data: [0, 0, 0, 8809]
+		},{
+			name: 'M-PESA Agents',
+			data: [355, 2606, 9521, 18103]
+		}]
+	});
+
+var chart;
+	chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'technology2',
+			type: 'bar'
+		},
+		title: {
+		   text: 'Delivery Methods of Surveyed Products'
+		},
+		xAxis: {
+			categories: ['Agents', 'ATMs/kiosks', 'Branches', 'Passbook', 'Mobile phone', 'Debit card', 'Web'],
+			title: {
+				text: null
+			}
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				text: 'Number of products',
+			}
+		},
+		tooltip: {
+			formatter: function() {
+				return ''+
+					this.y;
+			}
+		},
+		plotOptions: {
+			bar: {
+				dataLabels: {
+					enabled: true
+				}
+			}
+		},
+		credits: {
+			enabled: false
+		},
+			series: [{
+			name: '# Products',
+			data: [15,24,52,10,31,20,14]
+		}, {
+			name: '# Institutions',
+			data: [6,14,17,3,15,7,7]
+		}]
+	});	
+
+var chart;
+	chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'commercial1',
+			type: 'line',
+		},
+		title: {
+			text: 'Kenya Commercial Bank',
+		},
+		subtitle: {
+			text: null,
+		},
+		xAxis: {
+			categories: ['2006','2007','2008','2009','2010']
+		},
+		yAxis: {
+			title: {
+				text: null
+			},
+			max: 6000000,
+			min: 0,
+			plotLines: [{
+				value: 0,
+				width: 1,
+				color: '#808080'
+			}]
+		},
+		tooltip: {
+			formatter: function() {
+					return '<b>'+ this.series.name +'</b><br/>'+
+					this.x +': '+ this.y;
+			}
+		},
+		legend: {
+		},
+		series: [{
+			name: 'Balance Below KSh. 100,000',
+			data: [274840,402311,418942,654808,1214231]
+		}, {
+			name: 'Balance Above KSh. 100,000',
+			data: [69424,85356,87040,97044,126733]
+		}]
+	});
+
+var chart;
+	chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'commercial2',
+			type: 'line',
+		},
+		title: {
+			text: 'Equity Bank',
+		},
+		subtitle: {
+			text: null,
+		},
+		xAxis: {
+			categories: ['2006','2007','2008','2009','2010']
+		},
+		yAxis: {
+			title: {
+				text: null
+			},
+			max: 6000000,
+			min: 0,
+			plotLines: [{
+				value: 0,
+				width: 1,
+				color: '#808080'
+			}]
+		},
+		tooltip: {
+			formatter: function() {
+					return '<b>'+ this.series.name +'</b><br/>'+
+					this.x +': '+ this.y;
+			}
+		},
+		legend: {
+		},
+		series: [{
+			name: 'Balance Below KSh. 100,000',
+			data: [987435,1710684,2873920,3930363,5264244]
+		}, {
+			name: 'Balance Above KSh. 100,000',
+			data: [27039,129648,144436,107141,141488]
+		}]
+	});
+
+
+
+
+
+
+
+
+
+
+
+var chart;
 	chart = new Highcharts.Chart({
 		chart: {
 			renderTo: 'container',
@@ -73,142 +379,6 @@ $(document).ready(function() {
 		}, {
 			name: 'Balance Above KSh. 100,000',
 			data: [390233, 409812, 591448, 621869, 643629, 764457]
-		}]
-	});
-
-var chart;
-	chart = new Highcharts.Chart({
-		chart: {
-			renderTo: 'savings1',
-			plotBackgroundColor: null,
-			plotBorderWidth: null,
-			plotShadow: false
-		},
-		tooltip: {
-			formatter: function() {
-				return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
-			}
-		},
-		plotOptions: {
-			pie: {
-				allowPointSelect: true,
-				cursor: 'pointer',
-				dataLabels: {
-					enabled: true,
-					color: '#000000',
-					connectorColor: '#000000',
-					formatter: function() {
-						return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
-					}
-				}
-			}
-		},
-		series: [{
-			type: 'pie',
-			name: 'Browser share',
-			data: [
-				['Commercial Bank',   12],
-				['Postal Bank',       1],
-				['Microfinance Institution',    4],
-				['Credit Union/Cooperative',     1],
-				['Insurance Provider',   2],
-				['State Owned Bank',   1]
-			]
-		}]
-	});
-
-var chart;
-	chart = new Highcharts.Chart({
-		chart: {
-			renderTo: 'savings2',
-			plotBackgroundColor: null,
-			plotBorderWidth: null,
-			plotShadow: false
-		},
-		tooltip: {
-			formatter: function() {
-				return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
-			}
-		},
-		plotOptions: {
-			pie: {
-				allowPointSelect: true,
-				cursor: 'pointer',
-				dataLabels: {
-					enabled: true,
-					color: '#000000',
-					connectorColor: '#000000',
-					formatter: function() {
-						return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
-					}
-				}
-			}
-		},
-		series: [{
-			type: 'pie',
-			name: 'Browser share',
-			data: [
-				['Current Account',   75],
-				['Commitment Savings Account',       26],
-				['Recurring Standard Deposit Account',    3],
-			]
-		}]
-	});
-
-var chart;
-	chart = new Highcharts.Chart({
-		chart: {
-			renderTo: 'technology1',
-			type: 'line',
-		},
-		title: {
-		   text: 'Needs title'
-		},
-		subtitle: {
-			text: null,
-		},
-		xAxis: {
-			categories: ['2007','2008','2009','2010']
-		},
-		yAxis: {
-			title: {
-				text: null
-			},
-			plotLines: [{
-				value: 0,
-				width: 1,
-				color: '#808080'
-			}]
-		},
-		tooltip: {
-			formatter: function() {
-					return '<b>'+ this.series.name +'</b><br/>'+
-					this.x +': '+ this.y;
-			}
-		},
-		legend: {
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'top',
-			x: -10,
-			y: 100,
-			borderWidth: 0
-		},
-		series: [{
-			name: 'Commercial Bank Branches',
-			data: [740, 887, 990, 1016]
-		},{
-			name: 'MFI Branches',
-			data: [0, 0, 6, 47]
-		},{
-			name: '# ATMs',
-			data: [1012, 1325, 1717, 1979]
-		},{
-			name: 'Bank Agents',
-			data: [0, 0, 0, 8809]
-		},{
-			name: 'M-PESA Agents',
-			data: [355, 2606, 9521, 18103]
 		}]
 	});
 
@@ -258,162 +428,6 @@ var chart;
 		},{
 			name: 'Bank Agents',
 			data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1500, 1500, 3647, 6513, 7999, null]
-		}]
-	});
-});    
-
-var chart;
-$(document).ready(function() {
-	chart = new Highcharts.Chart({
-		chart: {
-			renderTo: 'technology2',
-			type: 'bar'
-		},
-		xAxis: {
-			categories: ['Agents', 'ATMs/kiosks', 'Branches', 'Passbook', 'Mobile phone', 'Debit card', 'Web'],
-			title: {
-				text: null
-			}
-		},
-		yAxis: {
-			min: 0,
-			title: {
-				text: 'Number of products',
-			}
-		},
-		tooltip: {
-			formatter: function() {
-				return ''+
-					this.y;
-			}
-		},
-		plotOptions: {
-			bar: {
-				dataLabels: {
-					enabled: true
-				}
-			}
-		},
-		legend: {
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'top',
-			x: -60,
-			y: 50,
-			floating: true,
-			borderWidth: 1,
-			backgroundColor: '#FFFFFF',
-			shadow: true
-		},
-		credits: {
-			enabled: false
-		},
-			series: [{
-			name: '# Products',
-			data: [15,24,52,10,31,20,14]
-		}, {
-			name: '# Institutions',
-			data: [6,14,17,3,15,7,7]
-		}]
-	});	
-});
-
-var chart;
-$(document).ready(function() {
-	chart = new Highcharts.Chart({
-		chart: {
-			renderTo: 'commercial1',
-			type: 'line',
-		},
-		title: {
-			text: 'Kenya Commercial Bank',
-		},
-		subtitle: {
-			text: null,
-		},
-		xAxis: {
-			categories: ['2006','2007','2008','2009','2010']
-		},
-		yAxis: {
-			title: {
-				text: null
-			},
-			plotLines: [{
-				value: 0,
-				width: 1,
-				color: '#808080'
-			}]
-		},
-		tooltip: {
-			formatter: function() {
-					return '<b>'+ this.series.name +'</b><br/>'+
-					this.x +': '+ this.y;
-			}
-		},
-		legend: {
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'top',
-			x: -10,
-			y: 100,
-			borderWidth: 0
-		},
-		series: [{
-			name: 'Balance Below KSh. 100,000',
-			data: [274840,402311,418942,654808,1214231]
-		}, {
-			name: 'Balance Above KSh. 100,000',
-			data: [69424,85356,87040,97044,126733]
-		}]
-	});
-});
-
-var chart;
-$(document).ready(function() {
-	chart = new Highcharts.Chart({
-		chart: {
-			renderTo: 'commercial2',
-			type: 'line',
-		},
-		title: {
-			text: 'Equity Bank',
-		},
-		subtitle: {
-			text: null,
-		},
-		xAxis: {
-			categories: ['2006','2007','2008','2009','2010']
-		},
-		yAxis: {
-			title: {
-				text: null
-			},
-			plotLines: [{
-				value: 0,
-				width: 1,
-				color: '#808080'
-			}]
-		},
-		tooltip: {
-			formatter: function() {
-					return '<b>'+ this.series.name +'</b><br/>'+
-					this.x +': '+ this.y;
-			}
-		},
-		legend: {
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'top',
-			x: -10,
-			y: 100,
-			borderWidth: 0
-		},
-		series: [{
-			name: 'Balance Below KSh. 100,000',
-			data: [987435,1710684,2873920,3930363,5264244]
-		}, {
-			name: 'Balance Above KSh. 100,000',
-			data: [27039,129648,144436,107141,141488]
 		}]
 	});
 });
